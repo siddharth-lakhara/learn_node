6a51ea08b3e2aa8d0815cb16f0321844c95cf6e8
lakhara <siddharth.lakhara@gmail.com>
2018-01-26 15:46:15 +0530

    4th exercise done

---
+++ asynchIO.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile(process.argv[2], (err, data)=>{
+	if (err)
+		console.error(err);
+	else {
+		let lines = data.toString().split('\n').length - 1;
+		console.log(lines);
+	}
+});
