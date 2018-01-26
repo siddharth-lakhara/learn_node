d2332e9f82bdbf5188a5966ef6ced4e51e8068b2
lakhara <siddharth.lakhara@gmail.com>
2018-01-26 16:03:36 +0530

    Better solution for juggling asynch

--- httpAsynch.js
+++ httpAsynch.js
@@ -1,51 +1,30 @@
 const http = require('http');
 
-const url1 = process.argv[2];
-const url2 = process.argv[3];
-const url3 = process.argv[4];
+let url = [process.argv[2], process.argv[3], process.argv[4]];
+let res = [];
+let done=0;
+for (let i=0; i<url.length; i++){
+	res[i] = '';
+	done++;
+	get(i);
+}
 
-let doneUrl1 = false;
-let doneUrl2 = false;
-let doneUrl3 = false;
-
-let data1 = '';
-let data2 = '';
-let data3 = '';
-
-http.get(url1, (response)=>{
-	response.on('data', (data)=>{
-		data1 += data.toString();
+function get(index){
+	http.get(url[index], (response)=>{
+		response.on('data', (data)=>{
+			res[index] += data.toString();
+		});
+		response.on('end', ()=>{
+			done--;
+			completed();
+		});
 	});
-	response.on('end', ()=>{
-		doneUrl1 = true;
-		completed();
-	});
-});
-
-http.get(url2, (response)=>{
-	response.on('data', (data)=>{
-		data2 += data.toString();
-	});
-	response.on('end', ()=>{
-		doneUrl2 = true;
-		completed();
-	});
-});
-
-http.get(url3, (response)=>{
-	response.on('data', (data)=>{
-		data3 += data.toString();
-	});
-	response.on('end', ()=>{
-		doneUrl3 = true;
-		completed();
-	});
-});
+}
 
 function completed(){
-	if (doneUrl1 && doneUrl2 && doneUrl3){
-		console.log(data1);
-		console.log(data2);
-		console.log(data3);
+	if (!done){
+		for (let i=0; i<url.length; i++){
+			console.log(res[i]);
+		}
 	}
 }
