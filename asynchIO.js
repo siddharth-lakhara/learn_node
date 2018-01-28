4a5047b0b1c0d44e7461dddebccb39a104625b5b
lakhara <siddharth.lakhara@gmail.com>
2018-01-28 21:29:59 +0530

    tests written for asynchIO

--- asynchIO.js
+++ asynchIO.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, data)=>{
-	if (err)
-		console.error(err);
-	else {
-		let lines = data.toString().split('\n').length - 1;
-		console.log(lines);
-	}
-});
+//asynch
+function calcLines(fileName, callback){
+	// fs.readFile(process.argv[2], (err, data)=>{
+
+	fs.readFile(fileName, (err, data)=>{
+		if (err){
+			return callback(err.code);
+			//console.error(err);
+		}
+		else {
+			let lines;
+			lines = data.toString().split('\n').length - 1;
+			console.log(lines);
+			return callback(null, lines);
+		}
+	});
+}
+
+module.exports = calcLines;
---
+++ asynchIO.test.js
@@ -0,0 +1,41 @@
+const asynch = require('./asynchIO');
+
+global.console = {
+	warn: jest.fn(),
+	log: jest.fn()
+};
+
+test('Testing testFile1.txt: ', ()=>{
+	let callback = (retData)=>{
+		expect(retData).toBe(6);
+	};
+	asynch('./testFile1.txt', callback);
+});
+
+test('Testing testFile2.txt: ', ()=>{
+	let callback = (retData)=>{
+		expect(retData).toBe(8);
+	};
+	asynch('./testFile2.txt', callback);
+});
+
+test('Testing with file that doesnt exist: ', ()=>{
+	let callback = (retData)=>{
+		expect(retData).toBe('ENOENT');
+	};
+	asynch('./testFile3.txt', callback);
+});
+
+test('Testing with null: ', ()=>{
+	let callback = (retData)=>{
+		expect(retData).toBe('ENOENT');
+	};
+	asynch(null, callback);
+});
+
+test('Testing with undefined: ', ()=>{
+	let callback = (retData)=>{
+		expect(retData).toBe('ENOENT');
+	};
+	asynch(undefined, callback);
+});
